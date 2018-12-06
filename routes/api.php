9c5a3aafdf7fd4d45229660bb6e2d1418de7d1b1
Marouane Souah <marouanesouah@gmail.com>
2018-12-06 21:01:42 +0100

    adding API

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,19 @@ use Illuminate\Http\Request;
 
 
 Route::get('test',function() {
-    return App\Category::get();
+    //get first level categories
+    $level_1 = App\Category::where('level',1)->get();
+
+    $level_items = [];
+    foreach($level_1  as $level) {
+        array_push($level_items, $level);
+        $nested_level =  App\Category::where('parent_id',$level->id)->orderBy("level")->get();
+        foreach($nested_level as $nested) {
+            $level_items[sizeof($level_items) - 1]->child = $nested;
+        }
+    }
+
+    return    $level_items;
 });
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
