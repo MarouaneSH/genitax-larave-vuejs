f1bb132da945066488448f5388f00b981b0fd5dd
Marouane Souah <marouanesouah@gmail.com>
2018-12-30 21:09:58 +0100

    change urls

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,8 +13,8 @@ Vue.use(SocialSharing);
 Vue.use(VueRouter);
 Vue.use(Vuetify)
 
-window.axios.defaults.baseURL ="http://genitax.test/api";
-// window.axios.defaults.baseURL ="http://142.93.227.137/api";
+// window.axios.defaults.baseURL ="http://genitax.test/api";
+window.axios.defaults.baseURL ="http://142.93.227.137/api";
 
 const router = new VueRouter({
     routes
